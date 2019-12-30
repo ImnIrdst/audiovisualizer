d1aa26e5c4ccab1acf82554ffec4b4bdaa2589c1
Iman Irandoost <imn@cafebazaar.ir>
2019-12-30 20:05:42 +0330

    Draw bars from middle

--- app/src/main/java/ir/imn/audiovisualizer/visualizer/AudioVisualizer.kt
+++ app/src/main/java/ir/imn/audiovisualizer/visualizer/AudioVisualizer.kt
@@ -5,6 +5,7 @@ import android.graphics.*
 import android.graphics.Paint.ANTI_ALIAS_FLAG
 import android.util.AttributeSet
 import android.view.View
+import kotlin.math.max
 import kotlin.random.Random
 
 class AudioVisualizer(context: Context, attrs: AttributeSet) : View(context, attrs) {
@@ -28,13 +29,19 @@ class AudioVisualizer(context: Context, attrs: AttributeSet) : View(context, att
 
         val barWidth = width / (bars * 2f)
         for (i in 0..bars) {
+            val amp = max(Random.nextFloat() * height, barWidth)
+
+            val horizontalOffset = barWidth / 2 // remedy for last empty bar
+
+            val startX = barWidth * i * 2
+            val endX = startX + barWidth
+
+            val midY = height / 2
+            val startY = midY - amp / 2
+            val endY = midY + amp / 2
+
             data.add(
-                RectF(
-                    barWidth * i * 2 + barWidth / 2,
-                    0f,
-                    barWidth * (i * 2 + 1) + barWidth / 2,
-                    Random.nextFloat() * height
-                )
+                RectF(startX + horizontalOffset, startY, endX + horizontalOffset, endY)
             )
         }
     }
